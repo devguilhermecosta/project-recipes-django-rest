7f686de9e90214c0317ef687c5279f90fac8f8d1
Guilherme Costa <guilherme.partic@gmail.com>
2023-03-17 00:21:02 -0300

    feat: created the variables for serializer

--- recipes/serializers.py
+++ recipes/serializers.py
@@ -1,5 +1,12 @@
 from rest_framework import serializers
-from . models import Recipe
+from . models import Recipe, Category
+from tag.models import Tag
+
+
+class TagSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    name = serializers.CharField(max_length=25)
+    slug = serializers.SlugField()
 
 
 class RecipeSerializer(serializers.Serializer):
@@ -19,6 +26,57 @@ class RecipeSerializer(serializers.Serializer):
     preparation = serializers.SerializerMethodField(
         method_name='any_method_name',
         )
+
+    # Serializando fields do tipo foreingkey.
+    # Usamos o método PrimaryKeyRelatedField.
+    # Esse método recebe como atributo uma queryset.
+    # Ele retorna por padrão o pk de cada objeto.
+    # Isso pode ou não ser interessante para a aplicação.
+    # Uma opção para modificar isso é criar uma nova variável
+    # de classe sendo uma instância de StringRelatedField e usar
+    # o atributo 'source'. Isso funcionará se o field do model
+    # tiver um método __str__ implementado. Isso retornará um item
+    # json com category e outro category_name.
+    # Agora, se a intenção for retornar somente o __str__,
+    # basta usar uma instância de StringRelatedField sem source
+    # e de mesmo nome do field do model.
+    # Ex.: category = serilizers.StringRelatedField()
+    category = serializers.PrimaryKeyRelatedField(
+        queryset=Category.objects.all(),
+    )
+    category_name = serializers.StringRelatedField(
+        source='category'
+    )
+
+    author = serializers.StringRelatedField()
+
+    # Serializando objetos com relationship type many-to-many.
+    # Serializamos o field com PrimaryKeyRelatedField.
+    # Deve receber como atributos uma queryset e many=True.
+    #
+    # Uma opção para tornar mais intuitivo é criar uma
+    # classe para serializar cada tag.
+    # Então criamos um novo field que será uma instância
+    # da nova classe, passando o source e many como atributos.
+    #
+    # Ex.:
+    #   class RecipeSerializer(serializers.Serializer):
+    #       id = serializers.IntegerField()
+    #       title = serializers.CharField(max_length=65)
+    #       description = serializers.CharField(max_length=165)
+    #
+    #   tag_objects = TagSerializer(
+    #       source='tags',
+    #       many=True,
+    #   )
+    tags = serializers.PrimaryKeyRelatedField(
+        queryset=Tag.objects.all(),
+        many=True,
+    )
+    tag_objects = TagSerializer(
+        source='tags',
+        many=True,
+    )
 
     def any_method_name(self, recipe: Recipe):
         return f'{recipe.preparation_time} {recipe.preparation_time_unit}'
